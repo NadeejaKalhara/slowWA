3f8e3fcaff684d6eedbd318f7ce46f78ef321a99
Nadeeja Kalhara <elkalhara@gmail.com>
2022-08-09 02:24:16 +0530

    make it better

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const bot = new TeleBot("5353629531:AAFxt8atCemi9FTIh8ovErmCeuPt7euxkt0");
 
 bot.start();
 
+
 bot.on('text', (msg) => {msg.reply.text(msg.text)
     const phones = ['+94718083058'];
     const message = msg.text;
